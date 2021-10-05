7cd5fc2d46f171e21afd583cc07649129e36af10
GitWray1 <jonathan.wray@nice.org.uk>
2021-10-05 13:27:00 +0000

    Commit 2

--- analysis/study_definition_rec1.py
+++ analysis/study_definition_rec1.py
@@ -7,33 +7,41 @@ from cohortextractor import (
     filter_codes_by_category,
     patients,
 )
+
+# Import our predefined codelists
 from codelists import *
 
-susp_or_confirmed_covid_codes = codelist_from_csv(
-    "codelists/opensafely-chronic-cardiac-disease.csv", system="ctv3", column="CTV3ID"
-)
-
-covid_advice_provided_codes = codelist_from_csv(
-    "codelists/opensafely-chronic-cardiac-disease.csv", system="ctv3", column="CTV3ID"
-)
-
+# Define the pandemic start date
+pandemic_start_date = "2020-02-01"
 
 # Define the study definition
 study = StudyDefinition(
 
-    # Define the study index date
-    index_date = "2020-03-21",
-
     # Define the dummy data behaviour
-    default_expectations = {},
+    default_expectations={
+        "date": {"earliest": "1970-01-01", "latest": "today"},
+        "rate": "uniform",
+        "incidence": 0.8,
+    },
+
+    # Select the date column for people with suspected or confirmed COVID (Used to get advice given after diagnosis)
+    susp_or_confirmed_covid_date=patients.with_these_clinical_events(
+        susp_or_confirmed_covid_codes,
+        on_or_after=pandemic_start_date,
+        find_first_match_in_period=True,
+        returning='Date',
+        date_format='YYYY-MM-DD'
+    ),
 
     # Define the study population
-    population = patients.with_these_clinical_events(
-        susp_or_confirmed_covid_codes, on_or_after=index_date, include_date_of_match = True
+    population=patients.with_these_clinical_events(
+        susp_or_confirmed_covid_codes, 
+        on_or_after=pandemic_start_date
     ),
 
     # Define the variables we are interested in
-    received_info = patients.with_these_clinical_events(
-        covid_advice_provided_codes, on_or_after = 
+    advice_provided=patients.with_these_clinical_events(
+        covid_advice_provided_codes, 
+        on_or_after=susp_or_confirmed_covid_date
     )
 )
