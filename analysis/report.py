42a4bc44f04f78cca90e9aea36bc6dac9a3c6dca
GitWray1 <jonathan.wray@nice.org.uk>
2021-10-06 11:16:28 +0000

    updating report file

--- analysis/report.py
+++ analysis/report.py
@@ -1,12 +1,28 @@
+#Import relevant packages
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
 
-rec1_df = pd.read_csv(
+# Read in the results dataframe produced from the study definition
+df = pd.read_csv(
     'output/input_rec1.csv',
-    parse_dates = True,
-    names = ['covid_date','advice_date','ID'],
-    header=0
+    parse_dates=True,
+    names=['covid_date', 'advice_date', 'ID'],
+    header=0,
+    na_values=['', 0]
     )
 
-rec1_df.to_csv('output/output_table.csv', index=False)
+# Create a list to keep all the information about the recs - will use to build the final dataframe
+rec_list = []
+
+# Extract all of the information for Recommendation 1.1
+Rec1_1_num = df[df.advice_date >= df.covid_date].advice_date.count()
+Rec1_1_num_missing = df['advice_date'].isna().sum() / len(df['advice_date'])
+Rec1_1_denom = df.dropna(subset=['covid_date', 'advice_date']).covid_date.count()
+Rec1_1_denom_missing = df['covid_date'].isna().sum() / len(df['covid_date'])
+Rec1_1_ratio = Rec1_1_num / Rec1_1_denom
+Rec1_1 = ['1.1', 'Advice given', Rec1_1_num, Rec1_1_num_missing, Rec1_1_denom, Rec1_1_denom_missing, Rec1_1_ratio]
+rec_list.append(Rec1_1)
+
+# Create a dataframe to summarize the results
+results_df = pd.DataFrame(rec_list, columns=['Rec number', 'Description', 'Numerator', '%Num_missing', 'Denominator', '%Denom_Missing', 'Num/Denom'])
+
+results_df.to_csv('output/results_table.csv', index=False)
